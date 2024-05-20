b854725b08788f5402477a57b3461bdc291d13bf
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-05-20 19:43:44 +0700

    Menambahkan akses modifier bersifat public ke dalam kelas dokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -6,8 +6,22 @@ class dokter;
 class pasien{
 public:
     string nama;
+    vector<pasien*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Dokter \""<< nama << "\" ada\n";
+    }
+    ~pasien(){
+         cout << "Dokter \""<< nama << "\" tidak ada\n";
+}
+void tambahDokter(dokter*);
+void cetakDokter(); 
+};
+
+class dokter{
+    public:
+    string nama;
     vector<pasien*> daftar_pasien;
-    
+
     dokter(string pNama) :nama(pNama) {
         cout << "Dokter \""<< nama << "\" ada\n";
     }
@@ -15,6 +29,29 @@ public:
          cout << "Dokter \""<< nama << "\" tidak ada\n";
 }
 void tambahPasien(pasien*);
-void cetakPasien(); 
+void cetakPasien();
 };
+
+void pasien ::tambahDokter(dokter* pDokter){
+    daftar_dokter.push_back(pDokter);
+}
+void pasien ::cetakDokter(){
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for(auto& a : daftar_dokter){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+void dokter ::tambahPasien(pasien* pPasien){
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter ::cetakPasien(){
+    cout << "Daftar Pasien dari doktor \"" << this->nama << "\":\n";
+    for(auto& a : daftar_pasien){
+        cout << a->nama << "\n";
+    }
+    cout << endl; 
+}  
+
 

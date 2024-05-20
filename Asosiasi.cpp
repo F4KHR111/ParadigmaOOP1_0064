abae3aab4a3952b9510e66b053bae74ebb3de8fe
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-05-20 19:33:22 +0700

    Menambahkan akses modifier bersifat public kedalam kelas pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 class dokter;
 class pasien{
-
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+    
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \""<< nama << "\" ada\n";
+    }
+    ~dokter(){
+         cout << "Dokter \""<< nama << "\" tidak ada\n";
+}
+void tambahPasien(pasien*);
+void cetakPasien(); 
 };
+
